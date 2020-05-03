68d685b3dc041dbc8322c950bd70a9a60523edc7
Boren Wang <borwang@cs.stonybrook.edu>
2020-05-02 20:43:01 -0400

    Finished server initialization

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -12,8 +12,12 @@
 #include "pbx.h"
 #include "server.h"
 #include "debug.h"
+#include "csapp.h"
 
 static void terminate(int status);
+static void sighup_handler();
+
+extern char *optarg;
 
 /*
  * "PBX" telephone exchange simulation.
@@ -21,9 +25,19 @@ static void terminate(int status);
  * Usage: pbx <port>
  */
 int main(int argc, char* argv[]){
+    int listenfd, *connfdp;
+    char* port;
+    socklen_t clientlen = sizeof(struct sockaddr_in);;
+    struct sockaddr_in clientaddr;
+    pthread_t tid;
+
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
+    int option = getopt(argc, argv, "p:");
+    if( option == 'p' ){
+        port = optarg;
+    }
 
     // Perform required initialization of the PBX module.
     debug("Initializing PBX...");
@@ -34,6 +48,18 @@ int main(int argc, char* argv[]){
     // run function pbx_client_service().  In addition, you should install
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
+    struct sigaction sa;
+    sa.sa_handler = sighup_handler;
+    if( sigaction(SIGHUP, &sa, NULL)==-1 ) {
+        debug("sigaction error");
+    }
+
+    listenfd = Open_listenfd(port);
+    while(1) {
+        connfdp = Malloc(sizeof(int));
+        *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Pthread_create(&tid, NULL, pbx_client_service, NULL);
+    }
 
     fprintf(stderr, "You have to finish implementing main() "
 	    "before the PBX server will function.\n");
@@ -49,4 +75,8 @@ void terminate(int status) {
     pbx_shutdown(pbx);
     debug("PBX server terminating");
     exit(status);
+}
+
+void sighup_handler() {
+    terminate(EXIT_SUCCESS); // ???
 }
